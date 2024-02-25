227db3a5461e97f7d17ee645395a266a79444003
Hovhannes <62969447+kukss1@users.noreply.github.com>
2024-02-25 16:09:59 +0400

    Animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
+import { useTransition, animated } from "react-spring";
+import { useMemo } from "react";
+
 import Main from "./components/body/Main";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
@@ -8,19 +10,39 @@ import Left from "./components/body/sidebars/left/Left";
 import Bond from "./components/body/bond/Bond";
 import Contact from "./components/body/contact/Contact";
 
+import "./App.css";
+
 function App() {
   const { pathname } = useLocation();
+
+  const transitions = useTransition(pathname, {
+    key: pathname,
+    from: { transform: `scale(0.5)` },
+    enter: { transform: `scale(1)` },
+    leave: { transform: `scale(0.4)` },
+    config: { delay: 500 },
+  });
+
+  const memoizedRoutes = useMemo(
+    () => (
+      <Routes>
+        <Route path="/" element={<Main />} key="/" />
+        <Route path="/about" element={<About />} key="/about" />
+        <Route path="/bonds" element={<Bond />} key="/bonds" />
+        <Route path="/contact" element={<Contact />} key="/contact" />
+      </Routes>
+    ),
+    []
+  );
+
   return (
     <div className="App">
       <Header />
       <div className="app_body">
         {pathname === "/" || pathname === "/about" ? <Left /> : null}
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/bonds" element={<Bond />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        {transitions((styles, item) => (
+          <animated.div style={styles}>{memoizedRoutes}</animated.div>
+        ))}
       </div>
       <Footer />
     </div>
